64273766e606cef57c78412ee93e2d95a4efb595
rishan.k <rishan.k@codilar.com>
2024-12-16 19:05:18 +0530

    added style and input component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  const a = 3; 
-  return (
-<div>{a}</div>
-  )
-}
+// function App() {
+//   const a = 'hi there'; 
+//   return (
+// <div>{a}</div>
+//   )
+// }
+const App = () => {
+  const[name, setName] = useState("")
+  return(
+    <div>
+      <input placeholder='Enter Your Name' 
+      onChange={(e)=> setName(e.target.value)}
+      />
+      {name}
+      <button className='btn'>ADD</button>
+    </div>
+  );
+};
 
 export default App;
